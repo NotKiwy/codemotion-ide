f645b95180492da7b172cf7dd573988cdc797c65
MotionCode <lolka0andrey@gmail.com>
2026-05-13 19:56:27 +0300

    Dynamic theme change optimization (Removed transition)

--- assets/js/handlers/themesHandler.js
+++ assets/js/handlers/themesHandler.js
@@ -4,14 +4,6 @@ export function optionsThemeButtonHandler(themeSelect) {
     themeSelect.on("click", (item) => {
         const id = item.id;
 
-        document.body.classList.add("theme-transition");
-
-        requestAnimationFrame(() => {
-            Setting.themeSelect(id);
-        });
-
-        setTimeout(() => {
-            document.body.classList.remove("theme-transition");
-        }, 400);
+        Setting.themeSelect(id);
     });
 }
